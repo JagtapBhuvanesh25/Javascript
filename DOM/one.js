917266eb5d294adcbd98001c43e67d5268d766cf
Bhuvanesh Jagtap <deshmukhkanha25@gmail.com>
2025-09-27 18:37:06 +0530

    DOM

---
+++ DOM/one.js
@@ -0,0 +1,10 @@
+    const pare = document.querySelector(".parent");
+    // console.log(pare);
+    // for (let i = 0; i < pare.children.length; i++) {
+    //     console.log(pare.children[i].innerHTML);
+    // }
+    pare.children[1].style.color = "orange";
+    // console.log(pare.firstElementChild.innerHTML);
+    // console.log(pare.lastElementChild.innerHTML);
+
+    //console.log(pare.childNodes);
